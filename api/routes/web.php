94d464e4b27d6c452d520fb96f9353ad1649cbc4
Dusan Mitic <dusanmitic774@gmail.com>
2025-06-23 13:15:48 +0200

    Remove message table & related code

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MessageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/message', [MessageController::class, 'index']);
-
-Route::post('/message', [MessageController::class, 'store']);
 
